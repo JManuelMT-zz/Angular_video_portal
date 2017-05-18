65996e1d28015d2f762def5514e549055d9838b4
jose mendez <josemanuelche93@gmail.com>
2017-05-18 01:45:31 -0500

    project ready

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -5,39 +5,28 @@ import {Router} from "@angular/router";
 import { showUserService, RouterMock, ActivatedRouterMock } from "../../testing/mock";
 import { RouterTestingModule } from '@angular/router/testing';
 
-fdescribe('HeaderComponent', () => {
+describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ HeaderComponent ],
-  //     providers: [
-  //       {provide: UserService, useValue: showUserService},
-  //       {provide: Router, useClass: RouterMock}
-  //     ]
-  //   })
-  //   .compileComponents();
-  // }));
+   beforeEach(async(() => {
+     TestBed.configureTestingModule({
+       declarations: [ HeaderComponent ],
+       providers: [
+         {provide: UserService, useValue: showUserService},
+         {provide: Router, useClass: RouterMock}
+       ]
+     })
+     .compileComponents();
+   }));
 
   beforeEach(() => {
-
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-      providers: [
-        {provide: UserService, useValue: showUserService},
-        {provide: Router, useClass: RouterMock}
-      ],
-      imports: [RouterTestingModule],
-    })
-
     fixture = TestBed.createComponent(HeaderComponent);
-    
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
-    //expect(component).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
